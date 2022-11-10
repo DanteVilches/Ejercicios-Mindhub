ea363200d4ff1d4877ef5066f8282c34d4dee011
Senkidev <dante.vilchesc@gmail.com>
2022-11-10 11:47:55 -0300

    commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let centimetros = document.querySelector(".centimetros");
 let kilogramos = document.querySelector(".kilogramos");
 let resultado = document.getElementById("resultado");
-
+let guardar = document.querySelector(".btn-success");
+let borrar = document.querySelector(".btn-danger");
+let inputTitulo = document.querySelector(".titulo");
+let inputTexto = document.querySelector(".textoNota");
 const formulario = document.forms[0];
 
 formulario.addEventListener("submit", (e) => {
@@ -9,21 +12,18 @@ formulario.addEventListener("submit", (e) => {
 	let estatura = (centimetros.value / 100) * (centimetros.value / 100);
 	let peso = kilogramos.value;
 
-	resultado.value = peso / estatura;
+	resultado.value = (peso / estatura).toFixed(2);
 });
 
 let peso = document.querySelector(".peso");
 let dolar = document.querySelector(".dolar");
-let conversor = document.querySelector(".conversor");
 
 peso.addEventListener("keyup", () => {
 	dolar.value = peso.value / 140;
-	console.log("hola");
 });
 
 dolar.addEventListener("keyup", () => {
 	peso.value = dolar.value * 140;
-	console.log("chao");
 });
 
 let notas = [];
@@ -45,21 +45,45 @@ notas.push(
 
 let idGlobal = notas[1].id;
 
-console.log(notas);
-
 let containerNotas = document.querySelector(".containerNotas");
 let fragment = document.createDocumentFragment();
-notas.forEach((element) => {
-	let div = document.createElement("div");
-	div.classList.add("card");
-	div.innerHTML = `<div class="card-body">
+
+function imprimirNota(array) {
+	containerNotas.innerHTML = "";
+	array.forEach((element) => {
+		let div = document.createElement("div");
+		div.classList.add("card");
+		div.innerHTML += `<div class="card-body">
 						<div class="cardHead bg-light">
 							<h5 class="card-title">${element.titulo}</h5>
 						</div>
 
 						<p class="card-text">${element.texto}</p>
-						<a href="#" class="btn btn-danger">Borrar Nota</a>
+						<a href="#" class="btn btn-danger close" onclick=borrarNota(${element.id})>Borrar Nota</a>
 					</div>`;
-	fragment.appendChild(div);
+		fragment.appendChild(div);
+	});
+	containerNotas.appendChild(fragment);
+}
+imprimirNota(notas);
+
+function agregarNota(newTitulo, newTexto) {
+	notas.push({
+		id: notas.length + 1,
+		titulo: newTitulo,
+		texto: newTexto,
+	});
+}
+
+guardar.addEventListener("click", (e) => {
+	e.preventDefault();
+	if (
+		!inputTexto.value.trim().length == 0 &&
+		!inputTitulo.value.trim().length == 0
+	) {
+		agregarNota(inputTitulo.value.trim(), inputTexto.value.trim());
+		imprimirNota(notas);
+	}
 });
-containerNotas.appendChild(fragment);
+
+function borrarNota(id) {}
